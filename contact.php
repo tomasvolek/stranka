8bb550925d28daf2e8098d5a391a79d530662107
tomasvolek <tomas.volek@ghotmail.com>
2017-03-06 20:17:07 +0100

    Adding my existing site.

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+
+// configure
+$from = 'Kontaktný formulár <tomas.volek@mgpartners.sk>'; 
+$sendTo = 'Kontaktný formulár <tomas.volek@mgpartners.sk>';
+$subject = 'Nový kontaktný formulár';
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
+$okMessage = 'Správa úspešne odoslaná, budeme Vás kontaktovať.';
+$errorMessage = 'Nastala chyba pri odoslaní formulára. Skúste neskôr, prosím.';
+
+// let's do the sending
+
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    mail($sendTo, $subject, $emailText, "From: " . $from);
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
